284961e268766d0625eb3309aeb5fa8ccff7fccf
Artem <korolya23fc@gmail.com>
2025-04-18 20:33:50 +0300

    Adding file ImageLoader and properties

--- TestProject/imageLoader.swift
+++ TestProject/imageLoader.swift
@@ -5,4 +5,21 @@
 //  Created by Artem on 18.04.25.
 //
 
-import Foundation
+import UIKit
+
+class ImageLoader {
+    private let proccessingQueue = DispatchQueue(
+        label: "image.proccessing.queue",
+        qos: .userInitiated,
+        attributes: .concurrent
+    )
+    
+    private let maxConcurrentTasks: Int
+    private let semaphore: DispatchSemaphore
+    
+    init(maxConcurrentTasks: Int = 3) {
+        self.maxConcurrentTasks = maxConcurrentTasks
+        self.semaphore = DispatchSemaphore(value: maxConcurrentTasks)
+    }
+    
+}
